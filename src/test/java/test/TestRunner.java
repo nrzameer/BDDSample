92b4a464362262ed9da2294ba79ee4382f2c4845
nrzameer <rzameer@connectall.com>
2020-07-10 20:19:50 +0530

    changes

--- src/test/java/test/TestRunner.java
+++ src/test/java/test/TestRunner.java
@@ -30,13 +30,15 @@ import cucumber.api.testng.TestNGCucumberRunner;
 //import io.cucumber.testng.TestNGCucumberRunner;
 //import io.cucumber.testng.*;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features="feature_files",
-glue= {"stepdefinitions"}, tags= {"@tsgT","@Test"},
-format=
+/*format=
 {"pretty",
 "html:target/cucumber-reports/cucumber-pretty",
-"json:target/cucumber-reports/CucumberTestReport.json"}
+"json:target/cucumber-reports/CucumberTestReport.json"} */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="feature_files",
+glue= {"stepdefinitions"}, tags= {"@tsgT","@Test"}
+
 )
 public class TestRunner {
 	 private TestNGCucumberRunner testNGCucumberRunner;
